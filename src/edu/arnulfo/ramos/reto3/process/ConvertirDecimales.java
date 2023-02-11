0ac5fe8a12d8c2f411801596e497ac0288bbb86f
Arnulfo-Ramos168 <arrsaurio@gmail.com>
2023-02-11 12:21:11 -0600

    reto 3

--- src/edu/arnulfo/ramos/reto3/process/ConvertirDecimales.java
+++ src/edu/arnulfo/ramos/reto3/process/ConvertirDecimales.java
@@ -4,13 +4,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class ConvertirDecimales {
+    /**
+     *
+     * @param decimal variable publica que recopilara un numero decimal dado por el usuario para ser convertido en ya sea un binario, octal o hexadecimal
+     * @param numBinario variable publica que sera llamada en el CLI, esta recopilara un numero de tipo binario para ser transformado a decimal
+     * @param numOctal variable publica que se llamara en el CLI para recopilar un valor de tipo octal para ser transformado en un decimal
+     * @param hexadecimal variable publica que es llamada edentro del CLI para recoilar un valor de tipo hexadecimal para ser transformado a decimal
+     *
+     */
     public static int decimal;
-    public static int digito;
     public static int numBinario;
     public static long numOctal;
-    public static String binario = "";
     public static String hexadecimal = "";
+
+    /**
+     * DecimalesABinario codigo especifico para transformar de numeros decimales a binario
+     */
     public static void DecimalesABinario(){
+        int digito;
+        String binario = "";
         do {
             digito = decimal % 2;
             binario += digito;
@@ -20,6 +32,10 @@ public class ConvertirDecimales {
         binario = new StringBuilder(binario).reverse().toString();
         System.out.println("Su numero es "+ binario + " en binario");
     }
+
+    /**
+     * DecimalesAOctal código específico para transformar numeros decimales en octales
+     */
     public static void DecimalesAOctal(){
         ArrayList<Integer> octal = new ArrayList<>();
         while (decimal != 0) {
@@ -33,6 +49,10 @@ public class ConvertirDecimales {
         }
         System.out.println();
     }
+
+    /**
+     * DecimalesAHexadecimales código específico para transformar números decimales a hexadecimales
+     */
     public static void DecimalesAHexadecimales(){
         int residuo;
         String hexadecimal = "";
@@ -45,6 +65,10 @@ public class ConvertirDecimales {
         System.out.println("Su numero en hexadecimal es: " + hexadecimal);
 
     }
+
+    /**
+     * BinariosADecimales código especifioco para transformar números binarios a decimales
+     */
     public static void BinariosADecimales(){
         int residuo, c = 0, decimal = 0;
         while (numBinario !=0){
@@ -56,6 +80,10 @@ public class ConvertirDecimales {
         }
         System.out.println(decimal);
     }
+
+    /**
+     * OctalesADecimales código específico para transformar números octales en decimales
+     */
     public static void OctalesADecimales(){
         int i = 0;
         long decimal = 0;
@@ -66,6 +94,10 @@ public class ConvertirDecimales {
         }
         System.out.println(decimal);
     }
+
+    /**
+     * HexadecimalesADecimales código específico para transformar números hexadecimales en decimales
+     */
     public static void HexadecimalesADecimales(){
         int decimal = 0;
         final String digitos = "0123456789ABCDEF";
