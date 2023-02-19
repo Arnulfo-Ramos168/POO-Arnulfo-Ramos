e3fc136f63ad6cd73aea39f823fee2540b92eca8
Arnulfo-Ramos168 <arrsaurio@gmail.com>
2023-02-18 22:12:52 -0600

    reto 4

--- src/edu/arnulfo/ramos/reto4/Main.java
+++ src/edu/arnulfo/ramos/reto4/Main.java
@@ -2,7 +2,17 @@ package edu.arnulfo.ramos.reto4;
 
 import edu.arnulfo.ramos.reto4.ui.CLI;
 
+/**
+ * En esta clase se encuentra el método main
+ *
+ * @autor Jonathan Peña
+ * @autor Arnulfo Ramos
+ */
 public class Main {
+    /**
+     *
+     * Método main que no contiene lógica
+     */
     public static void main(String[] args) {
        CLI.showMenu();
     }
--- src/edu/arnulfo/ramos/reto4/process/AnalizarVentas.java
+++ src/edu/arnulfo/ramos/reto4/process/AnalizarVentas.java
@@ -1,9 +1,24 @@
 package edu.arnulfo.ramos.reto4.process;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
+/**
+ *
+ * En esta clase se realizan las operaciones para obtener el trimestre con mayores ventas en general y por cadas sabor de café
+ *
+ * @autor Arnulfo Ramos
+ * @autor Jonathan Peña
+ */
 
 public class AnalizarVentas {
+    /**
+     *
+     * Este método se encarga de calcular el trimestre que mas vendió cada tipo de café
+     *
+     * @param ventas Es la tabla que contiene las ventas por semestre proveniente de CLI
+     *
+     * @return regresa el trimestre con mayor ventas por sabor, el mayor trimestre en total y los meses que abarca cada trimestre
+     */
     public static List<Integer> obtenerMaximaVentaPorTrimestre(int[][] ventas){
         double max;
         List<Integer> trimestres = new ArrayList<>();
@@ -21,22 +36,21 @@ public class AnalizarVentas {
         }
         return trimestres;
     }
-
-    public static List<Integer> obtenerMaximoTrimestre(int [][] ventas){
-        double max;
+    public static List<Integer> obtenerMayorTrimestre(int[][] ventas){
+        int maxTrimestre = 0;
+        int index = 0;
         List<Integer> trimestres = new ArrayList<>();
-        int auxIndex = 0;
         for (int i = 0; i < ventas.length; i++) {
-            max = Double.MIN_VALUE;
+            int sumatoria = 0;
             for (int j = 0; j < ventas.length; j++) {
-                if (ventas[j][i] > max) {
-                    max = ventas[i][j];
-                    auxIndex = j;
-                }
+                sumatoria += ventas[j][i];
             }
-
+            if (sumatoria > maxTrimestre){
+                maxTrimestre = sumatoria;
+                index = i;
+            }
         }
-        trimestres.add(auxIndex);
+        trimestres.add(index);
         return trimestres;
     }
 
