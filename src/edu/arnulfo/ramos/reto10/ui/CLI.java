82e0d2df669d30dcc6013161604de1b3aa6777a1
Arnulfo-Ramos168 <arrsaurio@gmail.com>
2023-04-13 01:41:27 -0600

    reto 10

--- src/edu/arnulfo/ramos/reto10/ui/CLI.java
+++ src/edu/arnulfo/ramos/reto10/ui/CLI.java
@@ -8,9 +8,49 @@ public class CLI    {
         CEO ceo1 = new CEO("Jessica", "Gonzales", "Ramos", "Global", 334861821, 2000);
         HR hr1 = new HR("Andrea", "Salcedo", "Ortiz", 524818624, 3027 );
 
-        JrEngineer jr2 = new SrEngineer("Oscar", "Estrada", "Zepeda", "Phyton, Java Strip, C, C++, HTML", 24479138, 27);
+
+        testEmpleado(jr1);
+        testEmpleado(sr1);
+        testEmpleado(mng1);
+        testEmpleado(ceo1);
+        testEmpleado(hr1);
+        System.out.println();
+
+        testProgrammer(jr1);
+        testProgrammer(sr1);
+        testProgrammer(mng1);
+        testProgrammer(ceo1);
+        testProgrammer(hr1);
+        System.out.println();
+
+        testInterviewer(jr1);
+        testInterviewer(sr1);
+        testInterviewer(mng1);
+        testInterviewer(ceo1);
+        testInterviewer(hr1);
 
 
+    }
+    public static void testEmpleado(Object object){
+        if (object instanceof Empleado){
+            System.out.println("El sujeto es un empleado: " + "" + object.getClass().getSimpleName());
+        } else {
+            System.out.println("El sujeto no es un empleado: " + "" + object.getClass().getSimpleName());
+        }
+    }
+    public static void testProgrammer(Object object){
+        if (object instanceof Programmer){
+            System.out.println("El empleado es un programador: " + "" + object.getClass().getSimpleName());
+        } else {
+            System.out.println("El empleado no es un programador: " + "" + object.getClass().getSimpleName());
+        }
+    }
 
+    public static void testInterviewer(Object object){
+        if (object instanceof Interviewer){
+            System.out.println("El empleado es un entrevistador: " + "" + object.getClass().getSimpleName());
+        } else {
+            System.out.println("El empleado no es un entrevistador: " + "" + object.getClass().getSimpleName());
+        }
     }
 }
